fb390f32237e7daee669d351417ebbc52198dd6a
lavaspoon <lavaspoon@gmail.com>
2024-11-07 22:19:33 +0900

    Logger 구현

---
+++ src/thread/util/MyLoggerMain.java
@@ -0,0 +1,10 @@
+package thread.util;
+
+import static thread.util.MyLogger.log;
+
+public class MyLoggerMain {
+    public static void main(String[] args) {
+        log("hello thread");
+        log(123);
+    }
+}
