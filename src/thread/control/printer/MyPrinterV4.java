9d2b002f6818fd03d735218af718435e283727ef
lavaspoon <lavaspoon@gmail.com>
2024-11-24 23:33:30 +0900

    Yield 양보 활용

--- src/thread/control/printer/MyPrinterV4.java
+++ src/thread/control/printer/MyPrinterV4.java
@@ -34,7 +34,9 @@ public class MyPrinterV4 {
         @Override
         public void run() {
             while (!Thread.interrupted()) {
+                System.out.println("!!!jobQueue = " + jobQueue);
                 if (jobQueue.isEmpty()) {
+                    System.out.println("양보");
                     Thread.yield(); // 큐가 없으면 양보
                     continue;
                 }
